37792f6c5195d01bc4335e5c9cb7879e5e31d7df
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-08-25 16:21:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,149 @@
+// JavaScript
+
+
+// variable :- 
+
+
+// var  - global scope , function scope
+
+// let  - block scope
+
+// const - block scope
+
+
+// rules
+
+
+// var 
+
+// 1. declration  // 2. intialiazation
+
+var a = 10
+
+// 3. reuse    // 4.reintialization
+
+    a = 12
+
+// 5. reDeclration
+
+var a = 13
+
+console.log(a)
+
+
+// let 
+
+// 1. declration // 2. reintialization
+
+let b = 15 
+
+// 3. reuse      // 4. reintialization
+
+    b = 16
+
+// 5. reDeclration 
+
+// let b 
+
+console.log(b);
+
+
+// const 
+
+// 1. declration  // intialization 
+
+const c = 17 
+
+// 3. reuse // reintialization
+
+    //   c = 18 
+    
+// 5. redeclration 
+
+// const c 
+
+console.log(c);
+
+
+
+// printing Statement
+
+// 1. console.log()
+
+let d = 55 
+
+console.log(d);
+console.log(100);
+
+
+
+// 2. alert()
+
+
+// let e = 1000
+// alert(e)
+
+// 3. confirm()
+
+// const f = "did you know programing ?"
+
+// confirm(f)
+
+// 4. prompt()
+
+
+// var name = "what is your name"
+
+// prompt(name)
+
+
+
+// 5. document.writeln()
+
+// let g = "hello"
+
+// document.writeln(g)
+
+
+
+// i want to print user age in console
+
+// let userAge = prompt("how old are you ?")
+
+// console.log(userAge);
+
+
+// i want to print pop way to user name 
+
+// let userName = prompt("what is your name ?")
+
+// alert(userName)
+
+// i want to print user name in ui
+
+// var userName = prompt("what is your name")
+
+// document.writeln(userName)
+
+
+// console method
+
+
+// 1. console.log()
+
+console.log(122);
+
+
+// 2. console.warn()
+
+console.warn(122);
+
+
+// 3. console.error()
+
+console.error(122);
+
+
+// 4. console.clear()
+
+// console.clear()
